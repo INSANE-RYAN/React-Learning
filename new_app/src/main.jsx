344956f2cee6a53156c15e29ae8f5833c2eb0a40
Sarthak Gupta <guptasarthak7890@gmail.com>
2026-01-18 17:25:42 +0530

    New App

---
+++ new_app/src/main.jsx
@@ -0,0 +1,43 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom'
+import Layout from './layout.jsx'
+import { Home, About, Contact } from './components/index.js'
+
+// const router = createBrowserRouter([
+//   { 
+//     path: "", 
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "/",
+//         element: <Home />
+//       },
+//       {
+//         path: "/about",
+//         element: <About />
+//       },
+//       {
+//         path: "/contact",
+//         element: <Contact />
+//       }
+//     ]
+//   },
+// ]);
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+    </Route>
+  )
+)
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
